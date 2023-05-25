58028881e2b769564bcd101ea136a95b9b78e108
Gustavo Favero <gustavo.vfavero@gmail.com>
2023-05-25 00:11:51 -0300

    finishing the Sidebar component style

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,5 @@
+import { PencilLine } from 'phosphor-react';
+
 import styles from './Sidebar.module.css'
 
 export function Sidebar() {
@@ -9,12 +11,15 @@ export function Sidebar() {
        />
 
       <div className={styles.profile}>
+        <img className={styles.avatar} src="https://github.com/Gufvr.png" />
+
         <strong>Gustavo Favero</strong>
         <span>Web developer</span>
       </div>
 
       <footer>
         <a href="#">
+          <PencilLine size={20} />
           Editar seu perfil
         </a>
       </footer>
