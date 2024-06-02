dc67e5fce55b7811bc151794f8d055b805ae8a7f
GuFvr <gustavo.vfavero@gmail.com>
2024-06-01 23:21:13 -0300

    chore: Import Ignite logo asset
    - Imported Ignite logo from assets directory.

--- src/App.jsx
+++ src/App.jsx
@@ -25,24 +25,6 @@ const posts = [
     ],
     publishedAt: new Date("2023-05-8 21:00:00"),
   },
-  {
-    id: 2,
-    author: {
-      avatarUrl: "https://github.com/diego3g.png",
-      name: "Diego Fernandes",
-      role: "CTO @Rocketseat",
-    },
-    content: [
-      { type: "paragraph", content: "Fala galeraa 👋" },
-      {
-        type: "paragraph",
-        content:
-          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
-      },
-      { type: "link", content: "jane.design/doctorcare" },
-    ],
-    publishedAt: new Date("2023-05-30 21:00:00"),
-  },
 ];
 
 export function App() {
